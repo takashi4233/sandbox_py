3350164d0e294be7fea87309dc98b6077bae9112
taksahi <takashi4233@gmail.com>
2020-01-22 22:57:58 +0900

    delete unuse files.

--- ClassSample/animal.py
+++
@@ -1,7 +0,0 @@
-class animal:
-    def move(self):
-        print ("👣")
-
-if __name__ == "__main__":
-    a = animal()
-    a.move()
--- ClassSample/cat.py
+++
@@ -1,9 +0,0 @@
-import animal as animal
-class cat(animal.animal):
-    def say(self):
-        return "にゃー😺"
-
-if __name__ == "__main__":
-    c = cat()
-    c.say()
-    c.move()
